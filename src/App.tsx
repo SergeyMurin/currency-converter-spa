33a0653d7dd88c54300e01b268d9e36472028f75
Sergey <sergeimurin29@gmail.com>
2022-12-01 17:47:54 +0300

    Init requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Converter from "./components/converter";
 import HistoricalRates from "./components/historical-rates";
 import AvailableCurrencies from "./components/available-currencies";
+import {useAction} from "./hooks/use-action";
 
 
 const App = () => {
+    const {makeConversion} = useAction();
+    const {getAvailableCurrencies} = useAction();
+    const {getHistoricalRates} = useAction();
+    const initRequests = [getAvailableCurrencies, getHistoricalRates];
+    useEffect(() => {
+        let requestIndex = 0;
+        const interval = 1000;
+        const timer = setInterval(() => {
+            initRequests[requestIndex]();
+            if (requestIndex === initRequests.length - 1) {
+                clearInterval(timer);
+            }
+            requestIndex++;
+        },interval)
+    }, []);
     return (
         <div className="App">
             App
--- src/components/converter.tsx
+++ src/components/converter.tsx
@@ -4,12 +4,8 @@ import {useAction} from "../hooks/use-action";
 
 const Converter: React.FC = () => {
     const {loading_status} = useTypedSelector(state => state.converter);
-    const {makeConversion} = useAction();
-    useEffect(() => {
-        setTimeout(() => {
-            makeConversion();
-        }, 1100)
-    }, []);
+
+
     return (
         <div className={"converter"}>
             converter {loading_status ? <b>Loading...</b> : <></>}
